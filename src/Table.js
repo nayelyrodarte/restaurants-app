684a020edead75fadd38f36cb78c51e4b3908106
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-22 13:45:09 -0500

    Add table component

---
+++ src/Table.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <th>Restaurant</th>
+          <th>Location</th>
+          <th>Rating</th>
+          <th></th>
+        </thead>
+        <thbody></thbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
