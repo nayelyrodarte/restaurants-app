e6d8c86bde1b5a8dbb50d3834d47bfda5e236367
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-22 13:44:43 -0500

    Add get request config with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [sortBy, setSortBy] = useState('rating');
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        'https://cors-anywhere.herokuapp.com/https://recruiting-datasets.s3.us-east-2.amazonaws.com/data_melp.json'
+      )
+      .then((res) => {
+        console.log(res);
+        setData(res);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header>
-      Melp
-      </header>
+    <div className='App'>
+      <header>Melp</header>
     </div>
   );
 }
