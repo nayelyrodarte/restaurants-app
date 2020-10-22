a3acee0e79e322a4fc4ed0ad006fa80f9f015efb
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-22 16:38:34 -0500

    Import table into main component (App)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+
+import Table from './Table';
+
 import './App.css';
 
 function App() {
   const [sortBy, setSortBy] = useState('rating');
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState('false');
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(
         'https://cors-anywhere.herokuapp.com/https://recruiting-datasets.s3.us-east-2.amazonaws.com/data_melp.json'
       )
       .then((res) => {
-        console.log(res);
-        setData(res);
+        console.log(res.data);
+        setData(res.data);
+        setLoading(false);
       });
   }, []);
 
   return (
     <div className='App'>
-      <header>Melp</header>
+      <header>
+        <h1>Melp</h1>
+      </header>
+      {loading ? 'Cargando...' : <Table data={data} />}
     </div>
   );
 }
