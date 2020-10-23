b1a181142adde33c9db7d59529898378c1086146
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-22 20:09:46 -0500

    Add sort function

--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Table(data) {
-  const displayRestaurants = data.data.map((restaurant) => (
-    <tr>
+  const [sortData, sortedData] = useState(data.data);
+
+  const displayRestaurants = sortData.map((restaurant) => (
+    <tr key={restaurant.id}>
       <td>{restaurant.name}</td>
       <td>
         {restaurant.address.street} <br /> {restaurant.address.city}
@@ -12,14 +14,27 @@ function Table(data) {
     </tr>
   ));
 
+  const sort = (key) => {
+    const sorted = data.data.sort((a, b) => {
+      if (a[key] < b[key]) return -1;
+      if (a[key] > b[key]) return 1;
+      return 0;
+    });
+    sortedData([...sorted]);
+  };
+
   return (
     <div>
       <table>
         <thead>
           <tr>
-            <th>Restaurante</th>
+            <th>
+              <button onClick={() => sort('name')}>Sort</button>Restaurante
+            </th>
             <th>Dirección</th>
-            <th>Rating</th>
+            <th>
+              <button onClick={() => sort('rating')}>Sort</button>Rating
+            </th>
             <th></th>
           </tr>
         </thead>
