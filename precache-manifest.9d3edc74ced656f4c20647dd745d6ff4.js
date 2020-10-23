a2dd9064fd3485faa615acff6a671837d5d880ef
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-23 13:05:27 -0500

    Updates

---
+++ precache-manifest.9d3edc74ced656f4c20647dd745d6ff4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73deed3057b4311a4b6b23d5989b60d",
+    "url": "/restaurants-app/index.html"
+  },
+  {
+    "revision": "5938ea894502a7ce674e",
+    "url": "/restaurants-app/static/css/main.5e4bb2da.chunk.css"
+  },
+  {
+    "revision": "52b52c295cac7e81f338",
+    "url": "/restaurants-app/static/js/2.dcc948ff.chunk.js"
+  },
+  {
+    "revision": "1ea24419ad7a840e55994e62f8788228",
+    "url": "/restaurants-app/static/js/2.dcc948ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5938ea894502a7ce674e",
+    "url": "/restaurants-app/static/js/main.df7f07a3.chunk.js"
+  },
+  {
+    "revision": "c60820860e14836a897c",
+    "url": "/restaurants-app/static/js/runtime-main.40580136.js"
+  },
+  {
+    "revision": "6c979a7eb809d18eb0d84bd29b2faec7",
+    "url": "/restaurants-app/static/media/alphabetical-order.6c979a7e.svg"
+  }
+]);
