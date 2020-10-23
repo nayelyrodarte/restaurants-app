8713a93ab0960f8b43bb27fcfba2ebf3ca2658e6
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-10-23 11:12:16 -0500

    Update MapContainer exports

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,26 +3,25 @@ import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 const mapStyles = {
   width: '100%',
-  height: '100%'
+  height: '100%',
 };
 
 function MapContainer() {
-  return <div>
-       <Map
+  return (
+    <div>
+      <Map
         google={this.props.google}
         zoom={14}
         style={mapStyles}
-        initialCenter={
-          {
-            lat: -1.2884,
-            lng: 36.8233
-          }
-        }
+        initialCenter={{
+          lat: -1.2884,
+          lng: 36.8233,
+        }}
       />
-  </div>;
+    </div>
+  );
 }
 
-export default {Map, GoogleApiWrapper({
-    apiKey: 'YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE'
-  })(MapContainer)}
-
+export default GoogleApiWrapper({
+  apiKey: 'YOUR_GOOGLE_MAPS_API_KEY_GOES_HERE',
+})(MapContainer);
